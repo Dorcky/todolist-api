f736d4f0466c5b6c1a15e7f952fbe2767995ba29
Dorcky <161785219+Dorcky@users.noreply.github.com>
2024-09-23 16:12:50 -0400

    Add todo routes and controllers

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, (error) => {
+  if (error) {
+    console.error('Error starting the server:', error);
+  } else {
+    console.log(`Server running on port ${PORT}`);
+  }
 });
